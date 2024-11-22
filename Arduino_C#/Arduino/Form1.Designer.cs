3c622c3234b8841778bc72861984f02d4fb4a86a
Luis Enrique Betancourt <lbetancourt894@gmail.com>
2024-11-22 15:42:50 -0600

    Codigo de Arduino y proyecto de C#

---
+++ Arduino_C#/Arduino/Form1.Designer.cs
@@ -0,0 +1,87 @@
+﻿namespace Arduino
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.BtnCon = new System.Windows.Forms.Button();
+            this.BtnDiscon = new System.Windows.Forms.Button();
+            this.txtAlgo = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // BtnCon
+            // 
+            this.BtnCon.Location = new System.Drawing.Point(29, 203);
+            this.BtnCon.Name = "BtnCon";
+            this.BtnCon.Size = new System.Drawing.Size(75, 23);
+            this.BtnCon.TabIndex = 0;
+            this.BtnCon.Text = "Conectar";
+            this.BtnCon.UseVisualStyleBackColor = true;
+            this.BtnCon.Click += new System.EventHandler(this.BtnCon_Click);
+            // 
+            // BtnDiscon
+            // 
+            this.BtnDiscon.Location = new System.Drawing.Point(156, 203);
+            this.BtnDiscon.Name = "BtnDiscon";
+            this.BtnDiscon.Size = new System.Drawing.Size(93, 23);
+            this.BtnDiscon.TabIndex = 1;
+            this.BtnDiscon.Text = "Desconectar";
+            this.BtnDiscon.UseVisualStyleBackColor = true;
+            this.BtnDiscon.Click += new System.EventHandler(this.BtnDiscon_Click);
+            // 
+            // txtAlgo
+            // 
+            this.txtAlgo.Location = new System.Drawing.Point(29, 120);
+            this.txtAlgo.Name = "txtAlgo";
+            this.txtAlgo.Size = new System.Drawing.Size(220, 20);
+            this.txtAlgo.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(295, 262);
+            this.Controls.Add(this.txtAlgo);
+            this.Controls.Add(this.BtnDiscon);
+            this.Controls.Add(this.BtnCon);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.FormConnection_FormClosed);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button BtnCon;
+        private System.Windows.Forms.Button BtnDiscon;
+        private System.Windows.Forms.TextBox txtAlgo;
+    }
+}
+
---
+++ Arduino_C#/Arduino/Form1.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.IO.Ports;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace Arduino
+{
+    public partial class Form1 : Form
+    {
+        System.IO.Ports.SerialPort Port;
+        bool isClosed = false;
+        string completo = "@-";
+        public Form1()
+        {
+            InitializeComponent();
+
+            Port = new System.IO.Ports.SerialPort();
+            Port.PortName = "COM6";  
+            Port.BaudRate = 9600;
+            Port.ReadTimeout = 500;
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Thread Hilo = new Thread(EsucharSerial);
+            Hilo.Start();
+        }
+        private void BtnCon_Click(object sender, EventArgs e)
+        {
+            BtnCon.Enabled = false;
+            BtnCon.SendToBack();
+
+            BtnDiscon.Enabled = true;
+            BtnDiscon.BringToFront();
+
+            try
+            {
+                Port.Open(); 
+
+                MessageBox.Show("Conexión establecida correctamente con el puerto.", "Conexión exitosa", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"No se pudo conectar al puerto: {ex.Message}", "Error de conexión", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void BtnDiscon_Click(object sender, EventArgs e)
+        {
+            BtnDiscon.Enabled = false;
+            BtnDiscon.SendToBack();
+
+            BtnCon.Enabled = true;
+            BtnCon.BringToFront();
+
+            try
+            {
+                Port.Close(); 
+
+                MessageBox.Show("Conexión Cerrada.", "Conexión cerrada", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"No se pudo cerrar el puerto: {ex.Message}", "Error al cerrar", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void EsucharSerial()
+
+        {
+            while (!isClosed)
+            {
+                try
+                {
+                    string cadena = Port.ReadLine();
+                    completo = completo.Insert(completo.Length - 1, cadena);
+
+
+                    txtAlgo.Invoke(new MethodInvoker(
+                        delegate
+                        {
+                            txtAlgo.Text = completo;
+                        }
+
+                        ));
+
+                }
+                catch { }
+            }
+
+        }
+
+
+
+        private void FormConnection_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            isClosed = true;
+        }
+    }
+}
+
